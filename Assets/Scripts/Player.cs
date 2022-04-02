070e90f127f8e46159e42033196a68d293d0dbb5
MoonWyv3rn <91739603+lucasoliveiradasilva@users.noreply.github.com>
2022-04-02 11:43:24 -0300

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,45 +5,37 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
  
-    public float speed ;
+    public float speedVertical;
+
+    public float speedHorizontal;
+
 
     public GameObject meteoro;
 
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position = transform.position + new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
+            transform.position = transform.position + new Vector3(0.0f, speedVertical * Time.deltaTime, 0.0f);
         }
           if (Input.GetKey(KeyCode.S))
         {
-            transform.position = transform.position + new Vector3(0.0f, -speed * Time.deltaTime, 0.0f);
+            transform.position = transform.position + new Vector3(0.0f, -speedVertical * Time.deltaTime, 0.0f);
         }
-         if (Input.GetKey(KeyCode.A))
+         if (Input.GetKey(KeyCode.D))
         {
-            transform.position = transform.position + new Vector3(speed, 0.0f * Time.deltaTime, 0.0f);
+            transform.position = transform.position + new Vector3(speedHorizontal, 0.0f * Time.deltaTime, 0.0f);
         }
-          if (Input.GetKey(KeyCode.D))
+          if (Input.GetKey(KeyCode.A))
         {
-            transform.position = transform.position + new Vector3(-speed, 0.0f * Time.deltaTime, 0.0f);
+            transform.position = transform.position + new Vector3(-speedHorizontal, 0.0f * Time.deltaTime, 0.0f);
         }
-
-
+    }
+    public void FixedUpdate()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
-          Instantiate(meteoro,new Vector3(3.0f,3.0f,0.0f),Quaternion.identity);
+            Instantiate(meteoro, new Vector3(3.0f, 3.0f, 0.0f), Quaternion.identity);
         }
-        
-
     }
 }
