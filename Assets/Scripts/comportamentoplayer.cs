8e731f991868a75483137a8a1a504f446e88c7e4
Azulrok <94077173+Azulrok@users.noreply.github.com>
2022-05-07 11:39:58 -0300

    01
    01

--- Assets/Scripts/comportamentoplayer.cs
+++ Assets/Scripts/comportamentoplayer.cs
@@ -11,15 +11,36 @@ public class comportamentoplayer : MonoBehaviour
     
     public float velocidademaxima = 10.0f;
     public Rigidbody2D player;
+    public float velocidadeProjetil = 3.0f;
+    public Rigidbody2D jogadorRb;
+
+     public Rigidbody2D projetoRb;
 
     void Start()
     {
         transform.position = new Vector3 (0.0f,0f,0f);
+        
+  
+    void start()
+    {
+       
+
+
+    }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+         if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody2D projetil = Instantiate(projetoRb,jogadorRb.position, Quaternion.identity);
+            projetil.velocity = transform.up * velocidadeProjetil;
+
+
+        }
+
         if(Input.GetKey(KeyCode.W))
         {
             Vector3 direcao = transform.up * aceleracao;
