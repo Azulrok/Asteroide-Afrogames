b0e69563451d26f43c3adbd0e20c37889034cc94
MoonWyv3rn <91739603+lucasoliveiradasilva@users.noreply.github.com>
2022-05-07 10:07:51 -0300

    Confia

--- Assets/Scripts/Ondas.cs
+++ Assets/Scripts/Ondas.cs
@@ -16,6 +16,9 @@ public class Ondas : MonoBehaviour
     {
         for (int i = 0; i < quantidadeAssteroide; i++)
         {
+            float x = Random.Range(-11.0f, 11.0f);
+            float y = Random.Range(-5.0f, 5.0f);
+
             Vector3 posicao = new Vector3(0.0f,0.0f,0.0f);
             Instantiate(prefabAsteroide, posicao,Quaternion.identity);
 
