84fabaa9929e530fb3b856a36b24dd3fe8f03d49
Azulrok <94077173+Azulrok@users.noreply.github.com>
2022-05-07 10:37:23 -0300

    01
    01

--- Assets/Scripts/Ondas.cs
+++ Assets/Scripts/Ondas.cs
@@ -16,9 +16,14 @@ public class Ondas : MonoBehaviour
     {
         for (int i = 0; i < quantidadeAssteroide; i++)
         {
+<<<<<<< HEAD
 
             float  x = Random.Range(-11.0f, 11.0f); //222
             float  y = Random.Range(-5.0f, 5.0f);
+=======
+            float x = Random.Range(-11.0f, 11.0f);
+            float y = Random.Range(-5.0f, 5.0f);
+>>>>>>> b0e69563451d26f43c3adbd0e20c37889034cc94
 
             Vector3 posicao = new Vector3(0.0f,0.0f,0.0f);
             Instantiate(prefabAsteroide, posicao,Quaternion.identity);
